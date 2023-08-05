771dbefdadf668279265da34d3ce961ea340e868
Avik Agarwala <avikagarwala@outlook.com>
2023-08-05 22:36:18 +0530

    create print-3.py

---
+++ basics/print-3.py
@@ -0,0 +1,10 @@
+print("I am\nAvik") # \n means new line
+
+print("Python is ", end = "") # default line break is no more
+print("FUN")
+
+print("Python is", end = " ")
+print("FUN")
+
+print("Python is ", end = "SUPER ")
+print("FUN")
