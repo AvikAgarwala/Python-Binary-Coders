8e6f2f9e33c9aba707d27623e570a96043fb5d62
Avik Agarwala <avikagarwala@outlook.com>
2023-08-05 21:09:12 +0530

    create variable.py

---
+++ basics/variable.py
@@ -0,0 +1,12 @@
+x = 5 # integer
+y = 5.43 # floating point number (float)
+z = "BCD_123@" # string
+m = 'T' # string
+b = True # boolean
+f = False # boolean
+
+print(x, y, z, m, b, f)
+
+m = 12 # changing data type
+
+print(m)
