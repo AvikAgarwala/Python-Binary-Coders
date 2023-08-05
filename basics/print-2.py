541c6003dfe903ba6827391d48aca1f3af6b10c2
Avik Agarwala <avikagarwala@outlook.com>
2023-08-05 21:51:57 +0530

    create print-3.py

---
+++ basics/print-2.py
@@ -0,0 +1,7 @@
+x = 404 # int
+
+print("Value of x is " + str(x))
+# in case of comma, type doesn't matter
+print("Value of x is", x) # by deafult a blank space in between
+# f string
+print(f"Value of x is {x}")
