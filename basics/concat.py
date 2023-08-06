ebff27b48d8a557bbbcd1d0fedc8e3c92fa8c036
Somnath Sengupta <136365858+SomnathSengupta@users.noreply.github.com>
2023-08-06 13:34:09 +0530

    Update concat.py
    I have added 4 comment lines

--- basics/concat.py
+++ basics/concat.py
@@ -1,7 +1,7 @@
-str1 = "Good"
-str2 = "Bye"
+str1 = "Good" # String 1
+str2 = "Bye"  # String 2
 
-print(str1 + str2)
-print(str1 + " " + str2)
+print(str1 + str2) # Concatenation with Methode 1
+print(str1 + " " + str2) # Concatenation with Methode 2
 # print(str1 + ' ' + str2) also works
 
