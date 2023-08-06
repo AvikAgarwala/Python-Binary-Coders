55034b6ebd2a28e0d90ef7945ac00f981f4fdf70
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-08-06 14:28:18 +0530

    Update input.py
    Adding some comments

--- basics/input.py
+++ basics/input.py
@@ -1,7 +1,7 @@
 # print("Enter a number:")
 # x = input()
 # x = int(x)
-x = int(input("Enter a number:"))
+x = int(input("Enter a number:")) # taking input
 
-print(type(x))
-print(x + 2)
+print(type(x)) # class is int 
+print(x + 2) # adding by 2
