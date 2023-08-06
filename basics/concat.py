7818aeeb66bcc0c0a2de104a542d798175fd522d
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-08-06 19:56:50 +0530

    Update concat.py

--- basics/concat.py
+++ basics/concat.py
@@ -3,5 +3,9 @@ str2 = "Bye"
 
 print(str1 + str2)
 print(str1 + " " + str2)
-# print(str1 + ' ' + str2)
-# print(str1 + ''' ''' + str2)
+''' more ways to do the same
+
+print(str1 + ' ' + str2)
+print(str1 + ''' ''' + str2)
+
+'''
