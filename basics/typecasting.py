81f1edc8153b17046feef01ee9c6c41d9f0bd218
Somnath Sengupta <136365858+SomnathSengupta@users.noreply.github.com>
2023-08-06 13:43:38 +0530

    Update typecasting.py

--- basics/typecasting.py
+++ basics/typecasting.py
@@ -1,6 +1,6 @@
 x = 5
 y = float(x) # typecasting is applied
-
+# ***The datatype of x havn't changed here.***
 print(type(x))
 print(type(y))
 
@@ -8,6 +8,6 @@ print(x)
 print(y)
 
 m = 3.67
-z = int(m)
-
+z = int(m) # typecasting is applied
+# ***The datatype of m havn't changed here.***
 print(z)
