6ab61c512c08bef5fa40f1b87c0c2d3306d94c1d
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-08-06 14:39:10 +0530

    Update input.py

--- basics/input.py
+++ basics/input.py
@@ -1,7 +1,7 @@
 # print("Enter a number:")
 # x = input()
 # x = int(x)
-x = int(input("Enter a number:")) # taking input
+x = int(input("Enter a number:")) # taking input and typecasting to int
 
-print(type(x)) # class is int 
-print(x + 2) # adding by 2
+print(type(x)) # int
+print(x + 2)
