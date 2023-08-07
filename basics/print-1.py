556971fc906dadbb7b64ec3742564902486dc90f
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-08-07 19:47:27 +0530

    Update print-1.py

--- basics/print-1.py
+++ basics/print-1.py
@@ -1,4 +1,4 @@
 print("Hello")
 print('Bye')
-print('''Good''') # We can write with three single quotations to print any string and also can be used for multi-line printing with single print function
+print('''Good''')
 print(12)
