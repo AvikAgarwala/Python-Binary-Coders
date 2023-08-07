b2a58be6638b320b8ebc4d83fc6f150df070c7a0
Priyam Dutta <113710032+Priyam123dutta@users.noreply.github.com>
2023-08-07 19:50:10 +0530

    Update print-2.py
    added comment

--- basics/print-2.py
+++ basics/print-2.py
@@ -4,4 +4,5 @@ print("Value of x is " + str(x))
 # in case of comma, type doesn't matter
 print("Value of x is", x) # by deafult a blank space in between
 # f string
-print(f"Value of x is {x}")
+print(f"Value of x is {x}") 
+# If we don't write 'f' before quotation then it will print as it is, it would never understand the meaning of giving { } within string
