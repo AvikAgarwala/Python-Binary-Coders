66d5f374a0275e4d39d40ae7fe114f5ee0be982f
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-08-09 14:42:51 +0530

    Update concat.py

--- basics/concat.py
+++ basics/concat.py
@@ -1,7 +1,6 @@
-str1 = "Good" # String 1
-str2 = "Bye"  # String 2
+str1 = "Good"
+str2 = "Bye"
 
-print(str1 + str2) # Concatenation with Methode 1
-print(str1 + " " + str2) # Concatenation with Methode 2
-# print(str1 + ' ' + str2) also works
-
+print(str1 + str2)
+print(str1 + " " + str2)
+# print(str1 + ' ' + str2)
